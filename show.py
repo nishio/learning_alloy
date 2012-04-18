c24f8afe0a51eb22a6a5e1b4d727176329f5d5bf
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2012-04-18 19:19:06 +0900

    add join method

--- show.py
+++ show.py
@@ -26,7 +26,35 @@ class AlloyList(list):
         return AlloyList(
             args + [_int(args[c]) for c in cols] for args in self)
 
-def get(label):
+    def join(self, al):
+        assert isinstance(al, AlloyList)
+        return AlloyList(
+            lhs[:-1] + rhs[1:]
+            for lhs in self for rhs in al
+            if lhs[-1] == rhs[0])
+    
+def get_field(label):
     return AlloyList(
         [x.attrs["label"] for x in r.findAll("atom")]
-        for r in soup.find(label=label).findAll("tuple"))
+        for r in soup.find("field", label=label).findAll("tuple"))
+
+def get_sig(label):
+    return AlloyList(
+        [r.attrs["label"]]
+        for r in soup.find("sig", label=label).findAll("atom"))
+
+def get(label):
+    try:
+        return get_field(label)
+    except:
+        pass
+    try:
+        return get_sig("this/" + label)
+    except:
+        pass
+    try:
+        return get_sig(label)
+    except:
+        pass
+    return AlloyList([[label]])
+
