fa9cd5901b971558a7deede6ce75e603b0f54689
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2012-04-24 00:52:33 +0900

    mardar2

--- show.py
+++ show.py
@@ -11,68 +11,103 @@ def load(s=None):
 
 
 def _is_two_dim(obj):
-    if isinstance(obj, list):
+    if isinstance(obj, list) or isinstance(obj, UtilList):
         if isinstance(obj[0], list):
             return True
     return False
 
 
-class AlloyList(list):
+class UtilList(list):
+    """List with utility methods"""
     def filter(self, f):
-        return AlloyList(args for args in self if f(*args))
+        return UtilList(args for args in self if f(*args))
 
     def trans(self, *cols):
-        return AlloyList([args[c] for c in cols] for args in self)
+        return UtilList([args[c] for c in cols] for args in self)
 
     def sort(self, *cols):
-        return AlloyList(sorted(self, key=lambda x: [x[c] for c in cols]))
+        return UtilList(sorted(self, key=lambda x: [x[c] for c in cols]))
 
     def to_int(self, *cols):
         def _int(s):
             return int(re.search("\d+", s).group())
-        return AlloyList(
+        return UtilList(
             args + [_int(args[c]) for c in cols] for args in self)
 
     def join(self, al):
-        assert _is_two_dim(al)
-        return AlloyList(
+        assert _is_two_dim(al), "%r is not 2-dim list" % al
+        return UtilList(
             lhs[:-1] + rhs[1:]
             for lhs in self for rhs in al
             if lhs[-1] == rhs[0])
 
+    def bjoin(self, al):
+        "backword join. x.bjoin(y) equals y.join(x), Alloy's y.x and x[y]"
+        return al.join(self)
+
     def map(self, f):
-        return AlloyList(f(*args) for args in self)
+        return UtilList(f(*args) for args in self)
 
     def group_by(self, idx, f=lambda *x: x):
         assert isinstance(idx, int)
-        group = defaultdict(AlloyList)
+        group = defaultdict(UtilList)
         for r in self:
             group[r[idx]].append(r)
-        result = AlloyList()
+        result = UtilList()
         for key in sorted(group.keys()):
             result.append(f(key, group[key]))
         return result
 
     def reverse(self):
-        return AlloyList(list(reversed(r)) for r in self)
+        return UtilList(list(reversed(r)) for r in self)
 
     def print_unlines(self):
         print "\n".join(str(r) for r in self)
 
+    def add(self, ys):
+        """x.add(y) is Alloy's x + y
+        (I don't like operator overload)"""
+        return UtilList(self + [y for y in ys if y not in self])
+
+    def remove(self, ys):
+        """x.remove(y) is Alloy's x - y
+        (I don't like operator overload)"""
+        return UtilList(x for x in self if x not in ys)
+
+    def as_scaler(self):
+        assert len(self) == 1
+        assert len(self[0]) == 1
+        return self[0][0]
+
+    def one_to_one(self, ys):
+        assert len(self) == len(ys)
+        return UtilList(x + _ensure_list(y) for (x, y) in zip(self, ys))
+
+    def print_unwords(self, sep=", "):
+        print sep.join(str(r) for r in self)
+
+
+def _ensure_list(x):
+    if isinstance(x, list): return x
+    return [x]
+
 def get_field(label):
-    return AlloyList(
+    return UtilList(
         [x.attrs["label"] for x in r.findAll("atom")]
         for r in soup.find("field", label=label).findAll("tuple"))
 
+
 def get_skolem(label):
-    return AlloyList(
+    return UtilList(
         [x.attrs["label"] for x in r.findAll("atom")]
         for r in soup.find("skolem", label=label).findAll("tuple"))
 
+
 def get_sig(label):
-    return AlloyList(
+    return UtilList(
         [r.attrs["label"]]
         for r in soup.find("sig", label=label).findAll("atom"))
+
 
 def get(label):
     try:
@@ -89,5 +124,21 @@ def get(label):
         return get_sig(label)
     except:
         pass
-    return AlloyList([[label]])
+    return UtilList([[label]])
 
+
+def get_all_label():
+    return dict(
+        sig=[x.attrs["label"] for x in soup.findAll("sig")],
+        field=[x.attrs["label"] for x in soup.findAll("field")],
+        skolem=[x.attrs["label"] for x in soup.findAll("skolem")])
+
+
+def show_label():
+    data = get_all_label()
+    print "sig:"
+    print "\n".join("  %s" % label for label in data["sig"])
+    print "field:"
+    print "\n".join("  %s" % label for label in data["field"])
+    print "skolem:"
+    print "\n".join("  %s" % label for label in data["skolem"])
